ad8cb97727e1136af1c3edbc857f96deab9ea80d
geekhall <174821300@qq.com>
2022-02-24 19:39:58 +0800

    Test

---
+++ server/src/main/java/cn/geekhall/server/bean/Hello.java
@@ -0,0 +1,18 @@
+package cn.geekhall.server.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Hello
+ *
+ * @author yiny
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Hello {
+    private String id;
+    private String message;
+}
--- server/src/main/java/cn/geekhall/server/controller/HelloController.java
+++ server/src/main/java/cn/geekhall/server/controller/HelloController.java
@@ -1,6 +1,9 @@
 package cn.geekhall.server.controller;
 
+import cn.geekhall.server.bean.Hello;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,8 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @ResponseBody
     @GetMapping("/hello")
-    public String hello(){
-        return "Hello Spring Boot2";
+    public Hello hello(){
+        System.out.println("Hello method called.");
+        Hello greeting = new Hello("1", "Hello Spring Boot2");
+        return greeting;
     }
 }
